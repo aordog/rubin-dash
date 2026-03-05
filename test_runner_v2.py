7a638c79a84a5c1641f342bff7fce3682d50a35e
Anna Ordog <aordog@uwo.ca>
2026-03-05 15:15:23 -0500

    add long term forecast plot

--- test_runner_v2.py
+++ test_runner_v2.py
@@ -13,14 +13,15 @@ state = {
     "date": None,
     "fig1_html": "",
     "fig2_html": "",
+    "fig3_html": "",
     "table": None,
     "version": 0,
 }
 
 # Build the target list:
-ra_t_list  = [350.0, 340.0, 330.0, 330.0, 325.0]
-dec_t_list = [-7.0, -8.0, -7.5, -8.0, -7.0]
-r_ang_list = [1.5, 1.5, 1.5, 1.5, 1.5]
+ra_t_list  = [350.0, 340.0, 330.0]#, 330.0, 325.0]
+dec_t_list = [-7.0, -8.0, -7.5]#, -8.0, -7.0]
+r_ang_list = [1.5, 1.5, 1.5]#, 1.5, 1.5]
 name = ' '
 
 target_set = []
@@ -29,7 +30,7 @@ for ra_t, dec_t, r_ang in zip(ra_t_list, dec_t_list, r_ang_list):
     target.add_mask_grid()
     target_set.append(target)
 
-t_refresh = 60
+t_refresh = 30
 
 # ---------- Background data loop ----------
 def data_loop():
@@ -49,10 +50,12 @@ def data_loop():
         for target in target_set:
             target.get_metadata_rsv(date)
             target.lsstcam_mask(date)
+            target.count_target_visits(date)
 
         target_plots = VisitsFigures(target_set[0]) # DEFAULTS TO FIRST TARGET (0)
         fig1_html = target_plots.visits_maps(date, 'daily') # DEFAULTS TO SHOWING DAILY VISITS
-        fig2_html = target_plots.visits_plots()
+        fig2_html = target_plots.visits_plots('daily') # DEFAULTS TO SHOWING DAILY VISITS
+        fig3_html = target_plots.make_long_forecast_plot(date)
         table = SummaryTable(target_set).make_table()
 
         # Swap in the new data
@@ -60,6 +63,7 @@ def data_loop():
             state["date"]     = date
             state["fig1_html"] = fig1_html
             state["fig2_html"] = fig2_html
+            state["fig3_html"] = fig3_html
             state["table"]    = table
             state["version"] += 1
             state["updating"]    = False
@@ -79,6 +83,7 @@ def home():
         date     = state["date"]
         fig1_html = state["fig1_html"]
         fig2_html = state["fig2_html"]
+        fig3_html = state["fig3_html"]
         table    = state["table"]
         version  = state["version"]
         next_update = state.get("next_update", 0)
@@ -90,7 +95,7 @@ def home():
 
     table_html = table.to_html(classes="data-table", border=0, index=False)
     return render_template('index.html', date=date,fig1_html=fig1_html, fig2_html=fig2_html,
-                           table_html=table_html, version=version,
+                           fig3_html=fig3_html,table_html=table_html, version=version,
                            countdown_seconds=max(0, next_update - server_time))
 
 @app.route("/row_clicked", methods=["POST"])
@@ -105,9 +110,11 @@ def row_clicked():
 
     target_plots = VisitsFigures(target_set[index])
     fig1_html_new = target_plots.visits_maps(date, maptype)
-    fig2_html_new = target_plots.visits_plots()
+    fig2_html_new = target_plots.visits_plots(maptype)
+    fig3_html_new = target_plots.make_long_forecast_plot(date)
 
-    return jsonify({"status": "ok", "fig1_html": fig1_html_new, "fig2_html": fig2_html_new})
+    return jsonify({"status": "ok", "fig1_html": fig1_html_new, "fig2_html": fig2_html_new,
+                    "fig3_html": fig3_html_new})
 
 @app.route("/maptype_clicked", methods=["POST"])
 def maptype_clicked():
@@ -122,9 +129,9 @@ def maptype_clicked():
 
     target_plots = VisitsFigures(target_set[index])
     fig1_html_new = target_plots.visits_maps(date, maptype)
-    #fig2_html_new = target_plots.visits_plots()
+    fig2_html_new = target_plots.visits_plots(maptype)
 
-    return jsonify({"status": "ok", "fig1_html": fig1_html_new})
+    return jsonify({"status": "ok", "fig1_html": fig1_html_new, "fig2_html": fig2_html_new})
 
 @app.route("/check_update")
 def check_update():
